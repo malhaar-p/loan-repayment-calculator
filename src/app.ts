e1e2e2f9ce7000f92698ce4daad7a2ca488fd778
Malhaar Punjabi <Malhaar.Punjabi@digitalhealth.gov.au>
2021-01-11 16:01:15 +1000

    some changes

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,18 @@ export interface ILoan {
 }
 
 
+const calculate = (): void => {
+
+    let loanAmount:number = document.getElementById("loanAmount").value;
+    let interestRate:number = document.getElementById("rate").value;
+    let duration:number = document.getElementById("rate").value;
+
+
+
+    
+}
+
+
 function calcInterestMonthly(loanAmount: number, interestRate: number) : number {
 
     let totalInterest = (interestRate*loanAmount)/12;
